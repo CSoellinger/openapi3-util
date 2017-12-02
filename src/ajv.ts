9196306527209ab2fb846e5eb0f196018b855164
CSoellinger <cs@pixelcrab.at>
2017-12-02 01:25:06 +0100

    Upd

--- src/ajv.ts
+++ src/ajv.ts
@@ -2,6 +2,6 @@ import * as Ajv from 'ajv';
 import * as path from 'path';
 
 export const ajv = new Ajv();
-ajv.addMetaSchema(require(path.resolve(__dirname, './schemas/json-schema-draft-04.json')));
+ajv.addMetaSchema(require(path.resolve(__dirname, '..', 'schemas', 'json-schema-draft-04.json')));
 
-export const validateOpenApi3Schema = ajv.compile(require(path.resolve(__dirname, './schemas/openapi-v3.json')));
+export const validateOpenApi3Schema = ajv.compile(require(path.resolve(__dirname, '..', 'schemas', 'openapi-v3.json')));
